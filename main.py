5fc843fb8d040d48c12ea0608c88c53d4e00f9b3
Inzamam1234 <137611077+Inzamam1234@users.noreply.github.com>
2024-08-04 05:06:32 +0000

    html and css

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask('app')
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def hello():
+  return render_template('index.html')
 
 app.run(host='0.0.0.0', port=8080)
